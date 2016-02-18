21e3cde34986cdddbc1b9236cce758067350d037
dusan98 <dusan.radosavljevic82@gmail.com>
2016-02-18 12:38:48 +0100

    Update login.php

--- odesk/login.php
+++ odesk/login.php
@@ -5,7 +5,7 @@
 <h1 id="prijava"></h1>Prijava</h1>
 
 <form action="login_check.php" method="POST">
-    E-Pošta: <input type="email" name="email" /><br />
+    E-Pošta kurac : <input type="email" name="email" /><br />
     Geslo: <input type="password" name="pass" /><br />
     <input type="submit" value="Prijava" />
     <input type="reset" value="Prekliči" />
