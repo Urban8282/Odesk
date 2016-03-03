4daa9d7787b4836ed33ab3a4d66b98cc85d2f341
dusan98 <dusan.radosavljevic82@gmail.com>
2016-03-03 12:36:18 +0100

    Create sporocila_meni.php

---
+++ odesk/sporocila_meni.php
@@ -0,0 +1,75 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+		<link rel="stylesheet" type="text/css" href="blabla.css" />
+    </head>
+    <body>
+ <?php
+    include_once 'header.php';
+    include_once 'database.php';
+?>
+<form> 
+  
+  <div id="glavnimeni">
+  
+  <div id="sporočila">
+  SPOROČILA 
+  </div>
+  
+  <div id="Prijatelji">
+  PRIJATELJI 
+  </div>
+  
+  </div>
+  
+  <div id="spodnjimeni">
+  
+  <div id="izbira">
+  
+  <div id="novasporočila">
+  Nova sporočila
+  </div>
+  
+  <div id="prejeta">
+  Prejeta
+  </div>
+  <div id="poslana">
+  Poslana
+  </div>
+  <div id="osnutki">
+  Osnutki 
+  </div>
+  
+  </div>
+  
+   <div id="okno">
+  
+  </div> 
+  
+  <div id="opcije">
+  
+  <div id="izbriši">
+  Izbriši 
+  </div>
+  
+  <div id="uredi">
+  Uredi
+  </div>
+  </div>
+  
+  </div>
+  
+ 
+  
+  
+  
+  
+  
+ 
+
+<?php
+    include_once 'footer.php';
+?>
+    </body>
+</html>
