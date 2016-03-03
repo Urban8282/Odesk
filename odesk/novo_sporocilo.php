bb1e6dbe4515baa0b818d31fee9a19fb3c747a85
Urban8282 <urban.kconik@gmail.com>
2016-03-03 13:23:59 +0100

    Update novo_sporocilo.php

--- odesk/novo_sporocilo.php
+++ odesk/novo_sporocilo.php
@@ -2,15 +2,50 @@
     <head>
         <meta charset="UTF-8">
         <title></title>
-		<link rel="stylesheet" type="text/css" href="novo_sporocilo.css">
+		<link rel="stylesheet" type="text/css" href="novo_sporocilo.css" />
     </head>
     <body>
  <?php
     include_once 'header.php';
     include_once 'database.php';
-	
+
 ?>
-<div class="textbox">
+<form> 
+  
+  <div id="glavnimeni">
+  
+  <div id="sporočila">
+  SPOROČILA 
+  </div>
+  
+  <div id="Prijatelji">
+  PRIJATELJI 
+  </div>
+  
+  </div>
+  
+  <div id="spodnjimeni">
+  
+  <div id="izbira">
+  
+  <div id="novasporočila">
+  <a href="novo_sporocilo.php">Novo sporočilo</a>
+  </div>
+  
+  <div id="prejeta">
+  Prejeta
+  </div>
+  <div id="poslana">
+  Poslana
+  </div>
+  <div id="osnutki">
+  Osnutki 
+  </div>
+  
+  </div>
+  
+   <div id="okno">
+  <div class="textbox">
 <form> 
 
   Ime prejemnika: <input type="text" name="username" ><br>
@@ -18,15 +53,26 @@
   Soprejemniki: <input type="text" name="copy" ><br><br></div>
   
   Sporočilo:<br>
-  <textarea rows="14" cols="75"></textarea><br><br>
+  <textarea rows="14" cols="69"></textarea><br><br>
    <p align="middle">
   <input type="button" value="Pošlji sporočilo" />
 </p>
+  </div> 
+  
+  </div>
+  
+  </div>
+  
+ 
+  
+  
+  
+  
+  
+ 
 
 <?php
-	
-    include_once 'footer.php';
+   
 ?>
     </body>
 </html>
-
